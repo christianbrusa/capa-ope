542b505e8630f143624926b7b3e68e8eb049e864
christianbrusa <christianbrusa@hotmail.com>
2025-04-15 11:57:35 -0300

    Practica: sumo funciones para implementar/testear

--- ejercicios/ejercicio1.js
+++ ejercicios/ejercicio1.js
@@ -5,4 +5,15 @@ const deberiaRecargarNafta = ({ nafta, capacidadTanque }) => nafta < capacidadTa
 
 const necesitaCambiarAceite = ({ km }) => km > LIMITE_KM_CAMBIO_ACEITE;
 
+const calcularAutonomia = () => {
+    /* Implementar función que calcule la autonomía del vehículo en kms.
+    Sumarlo a la bateria de tests */
+}
+
+const verificarArranque = () => {
+    /* Implementar función que verifique si el vehículo puede arrancar.
+    Si no hay batería o nafta, debe lanzar la excepción correspondiente (crearlas).
+    Sumarlo a la bateria de tests probando los diferentes escenarios */
+}
+
 module.exports = { deberiaRecargarNafta, necesitaCambiarAceite };
